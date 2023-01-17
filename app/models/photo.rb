efe2266e1eaf2d59736dadbb805097936166c442
pelkeyuscg <kpelkey@chicagobooth.edu>
2023-01-17 13:23:24 +0000

    Added scopes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -27,4 +27,11 @@ class Photo < ApplicationRecord
   has_many :fans, through: :likes
   validates :caption, presence: true
   validates :image, presence: true
+
+  # app/models/photo.rb
+
+  scope :past_week, -> { where(created_at: 1.week.ago...) }
+
+  scope :by_likes, -> { order(likes_count: :desc) }
+  
 end
