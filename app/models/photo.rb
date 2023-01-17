1dc1c9e6c664fb1834c1bc021779ca645257bcae
pelkeyuscg <kpelkey@chicagobooth.edu>
2023-01-17 05:29:17 +0000

    Added validation

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -25,4 +25,6 @@ class Photo < ApplicationRecord
   has_many :comments
   has_many :likes
   has_many :fans, through: :likes
+  validates :caption, presence: true
+  validates :image, presence: true
 end
