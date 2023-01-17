4fb9aa9d6b6458f98cc94856a1faaf1d05844998
pelkeyuscg <kpelkey@chicagobooth.edu>
2023-01-17 13:30:19 +0000

    Added server logic

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -22,4 +22,8 @@
 class FollowRequest < ApplicationRecord
   belongs_to :recipient, class_name: "User"
   belongs_to :sender, class_name: "User"
+
+  # app/models/follow_request.rb
+
+  enum status: { pending: "pending", rejected: "rejected", accepted: "accepted" }
 end
