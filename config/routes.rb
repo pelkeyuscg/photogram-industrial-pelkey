fa915af4fa49c96bed20814af5dfcbe665bab431
pelkeyuscg <kpelkey@chicagobooth.edu>
2023-02-11 03:22:41 +0000

    Set up users#show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   root "photos#index"
 
+
+
   devise_for :users
 
   resources :comments  
   resources :follow_requests
   resources :likes
   resources :photos
+  resources :users, only: :show
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
